dfd644001373e943a8f382e3b332faf1e6619868
YuriyVorobyov96 <darthrevan1333@gmail.com>
2023-02-16 10:41:12 +0300

    added store

---
+++ mini-redux/src/store.ts
@@ -0,0 +1,29 @@
+export class Store {
+  private state: unknown;
+
+  private listeners: Function[] = [];
+
+  private reducer: Function;
+
+  constructor(reducer: Function, middleware?: Function) {
+    this.reducer = reducer;
+
+    if (middleware) {
+      middleware(Store)(reducer)
+    }
+  }
+
+  public subscribe(callback: Function): void {
+    this.listeners.push(callback);
+  };
+
+  public dispatch(action): void {
+    this.state = this.reducer(this.state, action);
+
+    this.listeners.forEach(listener => listener());
+  };
+
+  public getState() {
+    return this.state;
+  };
+}
